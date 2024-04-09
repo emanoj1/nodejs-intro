edeefd6197f5dab0a1cb85d0d2c1a622c904a5e3
Manoj Kumar <37259245+emanoj1@users.noreply.github.com>
2024-04-09 19:56:32 +1000

    feat: random pokemon generated

--- index.js
+++ index.js
@@ -1,2 +1,8 @@
 console.log("Terminal app is running!");
+
+const pokemon = require('pokemon');
+
+let randomPokemon = pokemon.random()
+console.log(`Today Pokemon is ${randomPokemon}`)
+
 console.log("Have fun! Enjoy!");
